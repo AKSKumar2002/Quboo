6c3e27107812833218a0cfa3594bc4680af24ee6
Mr. AKS <aksnetlink@gmail.com>
2025-11-09 10:27:52 +0530

    21

--- server/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendOtpToUser, verifyOtp } from '../controllers/otpController.js';
-
-const router = express.Router();
-
-router.post('/send', sendOtpToUser);
-router.post('/verify', verifyOtp);
-
-export default router;
